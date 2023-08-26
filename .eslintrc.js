b03cd5443521d09f25a67bc1788cc45b0db579be
Pablo S <opapito@github.com>
2023-08-25 20:02:05 -0700

    form block update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['info', 'unix'],
+    'linebreak-style': ['warn', 'unix'],
     'import/extensions': ['error', {
       js: 'always',
     }],
